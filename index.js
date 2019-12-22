60b50026433eec1f0ef53ff3bb71e007b546344e
Wolfbar <herrera.cristopher@gmail.com>
2019-12-21 21:10:43 -0300

    firs commit

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-console.log("hola mundo");
-
 const boton = window.document.querySelector('#boton');
 
 boton.addEventListener('click', mostrarDatos);
@@ -8,22 +6,23 @@ boton.addEventListener('click', mostrarDatos);
 function mostrarDatos () {
     
     const input1 = window.document.querySelector('#email');
-    const textoInput = input1.value;
+    const user = input1.value;
     const input2 = window.document.querySelector('#contraseña');
-    const textoInput2 = input2.value;
+    const pass = input2.value;
     
-
-    if (textoInput === 'ejemplo@gmail.com' & textoInput2 === '123456'){
-        console.log('Iniciando sesion')    
-    } else{
-        console.log('Email y/o contraseña incorrecta')
-    }
-    console.log(textoInput);
+    console.log(user);
     input1.value = '';
     input1.focus(); 
 
-    console.log(textoInput2);
+    console.log(pass);
     input2.value = '';
     input2.focus(); 
+
+    if (user === 'ejemplo@gmail.com' & pass === '123456'){
+        console.log('Iniciando sesion...')    
+    } else{
+        console.log('Email y/o contraseña incorrecta')
+    }
+    
 
 }
